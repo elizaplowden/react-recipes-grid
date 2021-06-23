00bc808ea43a0bd0d95e47605e4e46df62611016
Eliza Plowden <elizaplowden@gmail.com>
2021-06-23 12:28:44 +0100

    controlled element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: ''};
+
+  // onInputChange(e) {
+  //   e.target.value;
+
+  // }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Recipe Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({term: e.target.value})}
+            />
           </div>
         </form>
       </div>
